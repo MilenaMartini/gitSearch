1b579b2ff81820ef67b2b4ecd421a84e38b85c49
MilenaMartini <martinimilena8@gmail.com>
2023-03-06 15:08:42 -0300

    vai

---
+++ .history/src/components/User_20230302151007.tsx
@@ -0,0 +1,54 @@
+import { UserProps } from "../types/user"
+import{MdLocationPin} from "react-icons/md"
+import classes from "./Search.module.css";
+import {Link} from 'react-router-dom';
+
+let date_create;
+let date_now;
+
+
+const User = ({
+  avatar_url,
+        html_url,
+        bio,
+        login,
+        location,
+        followers,
+        created_at
+}: UserProps) => {
+
+  date_create = new Date(created_at);
+  date_now = new Date(Date.now());
+
+  return (
+    <div className={classes.user}>
+      <img src={avatar_url} alt={login} />
+      <h2>{login}</h2>
+      {location && (
+      <p className={classes.location}>
+        <MdLocationPin />
+        <span>{location}</span>
+      </p>
+      )}
+      <div className={classes.stats}>
+        <div>
+        <p className={classes.title}>Seguidores:</p>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <p className={classes.title}>Descrição:</p>
+          <p>{bio}</p>
+        </div>
+        <div>
+          <p className={classes.title}>Tempo de uso:</p>
+          <p >{date_now.getFullYear() - date_create.getFullYear()} anos, {date_create.getMonth() - date_now.getMonth()} meses </p>
+        </div>
+        <div className={classes.button}>
+             <Link to={html_url}>Ver Perfil</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User
